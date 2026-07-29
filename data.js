9c877a963f1f3a0ce00e2a9456d0c922c598fb3c
nabiyarystan-hash <nabiy.arystan@nu.edu.kz>
2026-07-29 16:33:04 +0500

    Update data.js

--- data.js
+++ data.js
@@ -1,42 +1,42 @@
 const antibiotics = [
   {
     name: "Amoxicillin",
-    map: "https://datawrapper.dwcdn.net/pRLQ2/1/"
+    map: "HOSPITAL_AMOXICILLIN_MAP_URL"
   },
   {
     name: "Ceftriaxone",
-    map: "https://datawrapper.dwcdn.net/eSsVt/1/"
+    map: "HOSPITAL_CEFTRIAXONE_MAP_URL"
   },
   {
     name: "Ciprofloxacin",
-    map: "https://datawrapper.dwcdn.net/N64iS/1/"
+    map: "HOSPITAL_CIPROFLOXACIN_MAP_URL"
   },
   {
     name: "Cefazolin",
-    map: "https://datawrapper.dwcdn.net/0nIAJ/2/"
+    map: "HOSPITAL_CEFAZOLIN_MAP_URL"
   },
   {
     name: "Azithromycin",
-    map: "https://datawrapper.dwcdn.net/Z85Hp/1/"
+    map: "HOSPITAL_AZITHROMYCIN_MAP_URL"
   },
   {
     name: "Levofloxacin",
-    map: "https://datawrapper.dwcdn.net/ka4LM/1/"
+    map: "HOSPITAL_LEVOFLOXACIN_MAP_URL"
   },
   {
     name: "Gentamicin",
-    map: "https://datawrapper.dwcdn.net/KhQ1T/1/"
+    map: "HOSPITAL_GENTAMICIN_MAP_URL"
   },
   {
     name: "Doxycycline",
-    map: "https://datawrapper.dwcdn.net/xUjJU/1/"
+    map: "HOSPITAL_DOXYCYCLINE_MAP_URL"
   },
   {
     name: "Ampicillin",
-    map: "https://datawrapper.dwcdn.net/h0a1r/1/"
+    map: "HOSPITAL_AMPICILLIN_MAP_URL"
   },
   {
     name: "Amoxicillin/Clavulanate",
-    map: "https://datawrapper.dwcdn.net/AyFxx/1/"
+    map: "HOSPITAL_AMOXICILLIN_CLAVULANATE_MAP_URL"
   }
 ];
