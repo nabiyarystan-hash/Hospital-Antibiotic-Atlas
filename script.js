888d3077d1d16b2f7c0268c791059d1bc669cf9d
nabiyarystan-hash <nabiy.arystan@nu.edu.kz>
2026-07-29 18:34:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -105,3 +105,70 @@ select.addEventListener("change", function () {
 
 
 });
+// Создание полной таблицы
+
+function createFullTable() {
+
+    const tableDiv = document.getElementById("fullTable");
+
+
+    let html = `
+
+    <table>
+
+        <tr>
+
+            <th>Region</th>
+            <th>Amoxicillin</th>
+            <th>Ceftriaxone</th>
+            <th>Ciprofloxacin</th>
+            <th>Cefazolin</th>
+            <th>Azithromycin</th>
+            <th>Levofloxacin</th>
+            <th>Gentamicin</th>
+            <th>Doxycycline</th>
+            <th>Ampicillin</th>
+            <th>Amoxicillin/Clavulanate</th>
+
+        </tr>
+
+    `;
+
+
+    tableData.forEach(row => {
+
+
+        html += `
+
+        <tr>
+
+            <td>${row.Region}</td>
+            <td>${row.Amoxicillin.toFixed(6)}</td>
+            <td>${row.Ceftriaxone.toFixed(6)}</td>
+            <td>${row.Ciprofloxacin.toFixed(6)}</td>
+            <td>${row.Cefazolin.toFixed(6)}</td>
+            <td>${row.Azithromycin.toFixed(6)}</td>
+            <td>${row.Levofloxacin.toFixed(6)}</td>
+            <td>${row.Gentamicin.toFixed(6)}</td>
+            <td>${row.Doxycycline.toFixed(6)}</td>
+            <td>${row.Ampicillin.toFixed(6)}</td>
+            <td>${row["Amoxicillin/Clavulanate"].toFixed(6)}</td>
+
+        </tr>
+
+        `;
+
+
+    });
+
+
+    html += `</table>`;
+
+
+    tableDiv.innerHTML = html;
+
+}
+
+
+
+createFullTable();
